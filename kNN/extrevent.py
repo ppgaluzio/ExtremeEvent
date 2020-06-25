8dd4ff0e905ea718c4ad8ab76298c91d7f6bbcd4
Paulo <galuzio.paulo@protonmail.com>
2020-06-25 11:29:21 -0400

    ENH: create ExtremeEvent class
    An ExtremeEvent object will hold the time series and the information
    regarding number of extreme events occurrence as well as sampling the
    appropriate regions

---
+++ kNN/extrevent.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+class ExtremeEvent(object):
+    """Reads time series and identifies extreme events information
+
+    """
+
+    def __init__(self):
+        self.t = None           # time array
+        self.x = None           # time series array
+        pass
+
+    def readts(self, filename):
+        """read the time series from `filename`
+
+        Parameters
+        ----------
+        filename : str
+            path to the file where the time series is stored, the data
+            must be saved in the following format:
+
+            t0 x0
+            t1 x1
+            t2 x2
+            ....
+
+        """
+        self.t, self.x = np.loadtxt(filename, unpack=True)
+        return self
+
+    def median(self):
+        """return the median of the time series
+        """
+        return np.median(self.x)
+
+    def std(self):
+        """return the standard deviation of the time series
+        """
+        return np.std(self.x)
+
+    # properties ##############################################################
+
+    def __len__(self):
+        return len(self.t)
+
+    @property
+    def dt(self):
+        return np.round(np.mean(self.t[1:] - self.t[:-1]), decimals=2)

63888dbaf51fc74e137ee12e8a683e81ba1ab703
Paulo <galuzio.paulo@protonmail.com>
2020-07-29 19:01:17 -0400

    dev: simple_extreme_event_test

--- kNN/test_extrevent.py
+++ kNN/test_extrevent.py
@@ -1,2 +1,22 @@
+import matplotlib.pyplot as pl
+
 from extrevent import ExtremeEventIdentifier
-from extrevent import find_ext_event_id_of_max
+from time_series import TimeSeriesContainer
+from time_series import plot_ts
+
+
+ts = TimeSeriesContainer(1)
+ts.read("nlse.dat")
+
+fig = plot_ts(ts.t, ts.x, threshold=ts.threshold)
+
+averagesize = 100
+ee = ExtremeEventIdentifier(ts, averagesize)
+coords = ee.select_X_y()
+
+for i in range(len(coords)):
+    index = coords[i, 0]
+    pl.plot(ts.t[index-averagesize:index+averagesize],
+            ts.x[index-averagesize:index+averagesize], 'b')
+
+pl.plot(ts.t[coords[:, 0]], ts.x[coords[:, 0]], 'ro')
